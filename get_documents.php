b3fabde7bf4ca5cad47396a26b05ab16e5f9b776
CalidadyNormalizacion <joseadalberto0572@gmail.com>
2025-01-10 17:03:06 -0600

    Update get_documents.php

--- get_documents.php
+++ get_documents.php
@@ -1,13 +1,10 @@
 <?php
+$host = '/cloudsql/calidad-447308:us-central1:documentmanagement';
+$user = 'root';       // Usuario configurado
+$password = 'n0m3l03101'; // Contraseña
+$dbname = 'documentmanagement';    // Nombre de la base de datos
 
-$conn = new mysqli(
-    '127.0.0.1',
-    'root',                  // Usuario de la base de datos
-    'n0m3l03101',         // Contraseña
-    'documentmanagement',       // Nombre de la base de datos
-    null,
-    '/cloudsql/calidad-447308:us-central1:documentmanagement'
-);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
